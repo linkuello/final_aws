cee21dc6ca438b82f715a38580eaecef1edc58f7
linkuello <144430394+linkuello@users.noreply.github.com>
2024-12-03 12:58:49 +0600

    Create aws-exports.js

---
+++ aws-exports.js
@@ -0,0 +1,11 @@
+const awsconfig = {
+    aws_project_region: 'us-east-2',
+    aws_cognito_identity_pool_id: 'us-east-2:XXXXXX-XXXX-XXXX-XXXX-XXXXXXXX',
+    aws_cognito_region: 'us-east-2',
+    aws_user_pools_id: 'us-east-2_30sbBON7D',
+    aws_user_pools_web_client_id: '3NZY2bsd1ttRbidLiyo8SJV4SHxsvpKh9M',
+    oauth: {},
+    federated: {},
+};
+
+export default awsconfig;
